4b5a17b4edf564747bf9fbdd5e0e0362fdf84a92
coloradomedia <piotrek.majkrzak@gmail.com>
2019-04-13 17:18:21 +0200

    divide by zero exception

---
+++ zad 2.4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Pobierz wartosc dzielnej (a) i dzielnika (b)
+                int a, b;
+                string text;
+                Console.Write("Podaj waetosc dzielnej (a): ");
+                text = Console.ReadLine();
+                Int32.TryParse(text, out a);
+                Console.Write("Podaj waetosc dzielnika (b): ");
+                text = Console.ReadLine();
+                Int32.TryParse(text, out b);
+
+                //Sprobuj wukonac dzielenie i obsluz wyjatek przy dzieleniu przez zero
+                Console.WriteLine("Wynik dzielenia {0}", a / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nie wolno dzielic przez zero!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
