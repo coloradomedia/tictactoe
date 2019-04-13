424ceb068da203fe9cbaa008bb5a88eca546b545
coloradomedia <piotrek.majkrzak@gmail.com>
2019-04-13 16:45:01 +0200

    First version of the program

--- kolkokrzyzyk/Program.cs
+++ kolkokrzyzyk/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace kolkokrzyzyk
 {
@@ -10,6 +6,22 @@ namespace kolkokrzyzyk
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = "x";
+            char znakGraczaB = "o";
+            char[,] plansza = new char[3, 3]
+            {
+                {'1','2','3' },
+                {'4','5','6' },
+                {'7','8','9' }
+            };
+
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz inie gracza B: ");
+            imieGraczaB = Console.ReadLine();
         }
+
     }
 }
