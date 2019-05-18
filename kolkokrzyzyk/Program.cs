1a242d35ee8055366ab95ad4484a751830d82d7d
coloradomedia <piotrek.majkrzak@gmail.com>
2019-05-18 17:42:15 +0200

    klasy

--- kolkokrzyzyk/Program.cs
+++ kolkokrzyzyk/Program.cs
@@ -6,10 +6,13 @@ namespace kolkokrzyzyk
     {
         static void Main(string[] args)
         {
-            string imieGraczaA = "";
-            string imieGraczaB = "";
-            char znakGraczaA = "x";
-            char znakGraczaB = "o";
+            GraczLudzki gA = new GraczLudzki();
+            GraczKomputerowy gB = new GraczKomputerowy();
+            gA.Imie = "Uzytkownik";
+            gB.Imie = "Komputer";
+            gA.Znak = 'o';
+            gB.Znak = 'x';
+
             char[,] plansza = new char[3, 3]
             {
                 {'1','2','3' },
@@ -17,11 +20,66 @@ namespace kolkokrzyzyk
                 {'7','8','9' }
             };
 
-            Console.Write("Wpisz imie gracza A: ");
-            imieGraczaA = Console.ReadLine();
-            Console.Write("Wpisz inie gracza B: ");
-            imieGraczaB = Console.ReadLine();
-        }
+            bool koniecGry = false;
+            bool ruchGraczaA = true;
+            while (!koniecGry)
+            {
+                Console.Clear();
+                RysujPlansze(plansza);
 
+                if (ruchGraczaA)
+                {
+                    Console.WriteLine("Ruch wykonuje: " + gA.Imie);
+                    koniecGry = gA.WykonajRuch(plansza);
+                    ruchGraczaA = false;
+                }
+                else
+                {
+                    Console.WriteLine("Ruch wykonuje: " + gB.Imie);
+                    koniecGry = gB.WykonajRuch(plansza);
+                    ruchGraczaA = true;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+    static void RysujPlansze(char[,] plansza)
+    {
+        int wysokosc = plansza.GetLength(0);
+        int szerokosc = plansza.GetLength(1);
+
+        for (int i = 0; i < wysokosc; ++i)
+        {
+            for (int j = 0; j < szerokosc; ++j)
+                Console.Write(plansza[i, j]);
+            Console.WriteLine();
+        }
+    }
+
+    interface IRuch
+    {
+        bool WykonajRuch(char[,] plansza);
+    }
+
+    abstract class Gracz
+    {
+        public string Imie { get; set; }
+        public char Znak { get; set; }
+    }
+
+    class GraczLudzki : Gracz, IRuch
+    {
+        public bool WykonajRuch(char[,] plansza)
+        {
+            return false;
+        }
+    }
+    class GraczKomputerowy : Gracz, IRuch
+    {
+        public bool WykonajRuch(char[,] plansza)
+        {
+            return false;
+        }
     }
 }
