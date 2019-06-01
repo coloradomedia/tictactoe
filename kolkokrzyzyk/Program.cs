7e0c3313493c7ce8ca018c9d2684560e97ae2b36
coloradomedia <piotrek.majkrzak@gmail.com>
2019-06-01 17:27:19 +0200

    tic tac toe

--- kolkokrzyzyk/Program.cs
+++ kolkokrzyzyk/Program.cs
@@ -6,29 +6,33 @@ namespace kolkokrzyzyk
     {
         static void Main(string[] args)
         {
-            string imieGraczaA = "";
-            string imieGraczaB = "";
-            char znakGraczaA = 'x';
-            char znakGraczaB = 'o';
-            char[,] plansza = new char[3, 3]
-            {
+            GraczKomputerowy gB = new GraczKomputerowy();
+            gA.Imie = "Uzytkownik";
+            gB.Imie = "Komputer";
+            gA.Znak = 'x';
+            gB.Znak = 'o';
+
+            char[,] plansza = new char[3, 3] {
                 {'1','2','3' },
                 {'4','5','6' },
                 {'7','8','9' }
             };
-
-            Console.Write("Wpisz imie gracza A: ");
-            imieGraczaA = Console.ReadLine();
-            Console.Write("Wpisz imie gracza B: ");
-            imieGraczaB = Console.ReadLine();
+            char[,] planszaKopia = plansza.Clone() as char[,];
 
             //petla na kolejne ruchy graczy
             bool koniecGry = false;
-            while (!koniecGry)
+            bool ruchGraczaA = true;
+            for (int runda = 0; runda < plansza.Length; ++runda)
             {
                 Console.Clear();
                 RysujPlansze(plansza);
 
+                if (ruchGraczaA)
+                {
+                    Console.WriteLine("Ruch wykonuje: " + gB.Imie);
+                    koniecGry = gB.WykonajRuch(plansza, planszaKopia);
+                    ruchGraczaA = true;
+                }
                 Console.ReadKey();
             }
         }
@@ -47,4 +51,73 @@ namespace kolkokrzyzyk
         }
 
     }
+    interface IRuch
+    {
+        bool
+    }
+    abstract class Gracz
+    {
+        public char Znak { get; set; }
+        public bool SprawdzCzyKoniecGry(char[,] plansza)
+        {
+            int wysokosc = plansza.GetLength(0);
+            int szerokosc = plansza.GetLength(1);
+            if (szerokosc != wysokosc)
+                throw new Exception("Plansza nie jest kwadratowa!");
+
+            //sprawdz wiersze
+            for (int i = 0; i < wysokosc; ++i)
+            {
+                int sumaWiersza = 0;
+                for (int j = 0; j < szerokosc; ++j)
+                {
+                    if (plansza[i, j] == Znak)
+                        ++sumaWiersza;
+                }
+                if (sumaWiersza == szerokosc)
+                    return true;
+            }
+            //sprawdz kolumny
+            for (int j = 0; j < szerokosc; ++j)
+            {
+                int sumaKolumny = 0;
+                for (int i = 0; i < szerokosc; ++i)
+                {
+                    if (plansza[i, j] == Znak)
+                        ++sumaKolumny;
+                }
+                if (sumaKolumny == szerokosc)
+                    return true;
+            }
+            //sprawdz przekatne
+            int sumaPrzekatnejA = 0;
+            int sumaPrzekatnejB = 0;
+            for (int k = 0; k < szerokosc; ++k)
+            {
+                if (plansza[k, k] == Znak)
+                    ++sumaPrzekatnejA;
+                if (plansza[k, szerokosc - 1 - k] == Znak)
+                    ++sumaPrzekatnejB;
+                }
+            if (sumaPrzekatnejA == szerokosc || sumaPrzekatnejB == szerokosc)
+                return true;
+            //zadna z opcji, czyli jeszcze nie koniec gry
+            return false;
+        }
+    }
+    class GraczLudzki : Gracz, IRuch
+    {
+        public bool WykonajRuch(char[,] plansza, char[,] planszaKopia)
+        {
+            return SprawdzCzyKoniecGry(plansza);
+        }
+    }
+    class GraczKomputerowy : Gracz, IRuch
+    {
+        public bool WykonajRuch(char[,] plansza, char[,] planszaKopia)
+        {
+            return SprawdzCzyKoniecGry(plansza);
+        }
+    }
+
 }
